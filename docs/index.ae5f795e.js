6e941749787210607944bead6ac33fe76c89e385
MarianoLibre <mariano.macri@mercadolibre.com>
2023-01-03 15:42:09 -0300

    test some shit

---
+++ docs/index.ae5f795e.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("root"),t=document.createElement("p");t.innerText="This is some more shit.",t.setAttribute("class","some-shit"),e.appendChild(t);
+//# sourceMappingURL=index.ae5f795e.js.map
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const root = document.getElementById("root")
+
+const test = document.createElement("p")
+test.innerText = "This is some more shit."
+test.setAttribute("class", "some-shit")
+root.appendChild(test)
+
