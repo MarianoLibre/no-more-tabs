7503207a9d2fea784965fcbc102350768f208865
MarianoLibre <mariano.macri@mercadolibre.com>
2023-01-03 18:40:57 -0300

    some more shit

--- src/index.js
+++ src/index.js
@@ -5,3 +5,8 @@ test.innerText = "This is some more shit."
 test.setAttribute("class", "some-shit")
 root.appendChild(test)
 
+const more = document.createElement("pre")
+more.className = "more-shit"
+more.innerText = "I fucking love BASH!"
+root.appendChild(more)
+
