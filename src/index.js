94b9ad926b554c13ce4413a3c9a185ffc7307d29
MarianoLibre <mariano.macri@mercadolibre.com>
2023-01-04 17:59:48 -0300

    Basic html and css + some icons

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-const root = document.getElementById("root")
-
-const test = document.createElement("p")
-test.innerText = "This is some more shit."
-test.setAttribute("class", "some-shit")
-root.appendChild(test)
-
-const more = document.createElement("pre")
-more.className = "more-shit"
-more.innerText = "I fucking love BASH!"
-root.appendChild(more)
 
